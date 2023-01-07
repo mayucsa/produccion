ed486ccf8f85b9f90aea8b995f04cc865de9164b
mayucsa <88587803+mayucsa@users.noreply.github.com>
2023-01-07 00:46:44 -0600

    inventario besser
    cambio en formato de inventario besser en cuarto de curado

--- trunk/implementacion/webapps/modulos/besser/curado/serverSidemp.php
+++ trunk/implementacion/webapps/modulos/besser/curado/serverSidemp.php
@@ -3,25 +3,24 @@ include_once "../../../dbconexion/conexion.php";
 // header('Content-Type: application/json');
 
 // DB table to use
-$table = 'seg_producto_bloquera';
+$table = 'seg_mp_bloquera';
  
 // Table's primary key
-$primaryKey = 'cve_bloquera';
+$primaryKey = 'cve_mp';
  
 // Array of database columns which should be read and sent back to DataTables.
 // The `db` parameter represents the column name in the database, while the `dt`
 // parameter represents the DataTables column identifier. In this case simple
 // indexes
 // $var    = "estatus_registro = 'VIG'";
-$var    = "area = 'Besser' AND  estatus_producto = 'VIG'";
+$var    = "area = 'Besser' AND  estatus_mp = 'VIG'";
 $columns = array(
-    array( 'db' => 'nombre_producto',   'dt' => 0 ),
-    array( 'db' => 'presentacion',      'dt' => 1 ),
-    array( 'db' => 'num_celdas',      'dt' => 2 ),
-    array( 'db' => 'cantidad',          'dt' => 3,
+    array( 'db' => 'nombre_materia_prima',   'dt' => 0 ),
+    array( 'db' => 'cantidad_materia_prima',    'dt' => 1,
                     'formatter' => function($d, $row){
-                        return number_format($d).' Pza';
-                    })
+                        return number_format($d).' '.$row[2];
+                    }),
+    array( 'db' => 'unidad_medida',   'dt' => 2 ),
 );
  
 // SQL server connection information
